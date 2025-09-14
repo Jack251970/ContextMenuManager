81ee0d4bc501f8d484010a5973d7f1290dc1e9ca
Jack251970 <1160210343@qq.com>
2025-09-15 01:00:39 +0800

    Fix background flickering

--- ContextMenuManager/BluePointLilac.Controls/LoadingDialog.Designer.cs
+++ ContextMenuManager/BluePointLilac.Controls/LoadingDialog.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using System.ComponentModel;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Windows.Forms;
 
 namespace BluePointLilac.Controls
@@ -106,18 +107,33 @@ namespace BluePointLilac.Controls
             this.SetStyle(ControlStyles.UserPaint, true);
         }
 
+        protected override void OnPaintBackground(PaintEventArgs pevent)
+        {
+            // None... Helps control the flicker.
+        }
         protected override void OnPaint(PaintEventArgs e)
         {
-            Rectangle rec = e.ClipRectangle;
+            const int inset = 2; // A single inset value to control teh sizing of the inner rect.
 
-            rec.Width = (int)(rec.Width * ((double)Value / Maximum)) - 4;
-            if (ProgressBarRenderer.IsSupported)
-                ProgressBarRenderer.DrawHorizontalBar(e.Graphics, e.ClipRectangle);
-            rec.Height = rec.Height - 4;
-            using (Brush backBrush = new SolidBrush(backColor))
-                e.Graphics.FillRectangle(backBrush, 2, 2, e.ClipRectangle.Width - 4, rec.Height);
-            using (Brush foreBrush = new SolidBrush(foreColor))
-                e.Graphics.FillRectangle(foreBrush, 2, 2, rec.Width, rec.Height);
+            using (Image offscreenImage = new Bitmap(this.Width, this.Height))
+            {
+                using (Graphics offscreen = Graphics.FromImage(offscreenImage))
+                {
+                    Rectangle rect = new Rectangle(0, 0, this.Width, this.Height);
+
+                    if (ProgressBarRenderer.IsSupported)
+                        ProgressBarRenderer.DrawHorizontalBar(offscreen, rect);
+
+                    rect.Inflate(new Size(-inset, -inset)); // Deflate inner rect.
+                    rect.Width = (int)(rect.Width * ((double)this.Value / this.Maximum));
+                    if (rect.Width == 0) rect.Width = 1; // Can't draw rec with width of 0.
+
+                    LinearGradientBrush brush = new LinearGradientBrush(rect, backColor, foreColor, LinearGradientMode.Vertical);
+                    offscreen.FillRectangle(brush, inset, inset, rect.Width, rect.Height);
+
+                    e.Graphics.DrawImage(offscreenImage, 0, 0);
+                }
+            }
         }
     }
 }
