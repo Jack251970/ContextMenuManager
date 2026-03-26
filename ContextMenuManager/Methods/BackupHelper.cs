9b7883ab509819548e56cda6075add2e81536e97
Jack251970 <1160210343@qq.com>
2026-03-26 11:51:27 +0800

    Update using directives in BackupHelper.cs
    Added Microsoft.Win32 using directive and removed System.Threading.Tasks to clean up and align dependencies in BackupHelper.cs.

--- ContextMenuManager/Methods/BackupHelper.cs
+++ ContextMenuManager/Methods/BackupHelper.cs
@@ -5,7 +5,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Xml;
 using System.Xml.Serialization;
 using static ContextMenuManager.Controls.ShellList;
